933e7ada4635c756b467c277db4f4d1c544071bc
Yuxin Zhou <yuxzho@microsoft.com>
2021-04-03 01:03:25 +0000

    Release 6.1.6

--- common/inc/fx_api.h
+++ common/inc/fx_api.h
@@ -26,7 +26,7 @@
 /*  APPLICATION INTERFACE DEFINITION                       RELEASE        */
 /*                                                                        */
 /*    fx_api.h                                            PORTABLE C      */
-/*                                                           6.1.5        */
+/*                                                           6.1.6        */
 /*  AUTHOR                                                                */
 /*                                                                        */
 /*    William E. Lamie, Microsoft Corporation                             */
@@ -57,6 +57,9 @@
 /*  03-02-2021     William E. Lamie         Modified comment(s), and      */
 /*                                            added standalone support,   */
 /*                                            resulting in version 6.1.5  */
+/*  04-02-2021     William E. Lamie         Modified comment(s), and      */
+/*                                            updated product constants,  */
+/*                                            resulting in version 6.1.6  */
 /*                                                                        */
 /**************************************************************************/
 
@@ -106,7 +109,7 @@ extern   "C" {
 #define AZURE_RTOS_FILEX
 #define FILEX_MAJOR_VERSION     6
 #define FILEX_MINOR_VERSION     1
-#define FILEX_PATCH_VERSION     5
+#define FILEX_PATCH_VERSION     6
 
 /* Define the following symbols for backward compatibility */
 #define EL_PRODUCT_FILEX
